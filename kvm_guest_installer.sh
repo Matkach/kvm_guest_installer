c8dfd9f3e34d871534955b3bedeca2808016ddb1
Matkach <34516217+Matkach@users.noreply.github.com>
2020-09-11 00:24:36 +0200

    Update kvm_guest_installer.sh

--- kvm_guest_installer.sh
+++ kvm_guest_installer.sh
@@ -2,49 +2,6 @@
 
 if [ "$EUID" -ne 0 ] ; then echo "Please run as root" ; exit ; fi
 
-cmd=(whiptail --separate-output --checklist "Select options:" 20 40 13)
-options=(
-         # any option can be set to default to "off"
-         1 "Ubuntu 16.04" off
-         2 "Ubuntu 18.04" off
-         3 "CentOS 7" off
-         4 "CentOS 8" off
-         5 "Debian 9" off
-         6 "Debian 10" off
-         7 "Fedora 31" off
-         8 "Fedora 32" off)
-choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-clear
-for choice in $choices
-do
-    case $choice in
-        1)
-            ubuntu16.04
-            ;;
-        2)
-            ubuntu18.04
-            ;;
-        3)
-            centos7
-            ;;
-        4)
-            centos8
-            ;;
-        5)
-            debian9
-            ;;
-        6)
-            debian10
-            ;;
-        7)
-            fedora31
-            ;;
-        8)
-            fedora32
-            ;;
-    esac
-done
-
 function questions {
   ##### Interactive questions
   echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
@@ -53,33 +10,35 @@ function questions {
   echo "You can run the script multiple times with deferent values to reach your goal"
   echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   echo -e "\e[32mKVM guest hostname:\e[39m "
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   read hostname
-  
+
   echo ""
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   echo -e "\e[32mKVM guest disk size (in GB):\e[39m "
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   read size
-  
+
   echo ""
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   echo -e "\e[32mKVM guest RAM Memory to allocate (in MB):\e[39m "
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   read ram
-    
+
   echo ""
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   echo -e "\e[32mEnter a number of virtual cpus:\e[39m "
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   read vcpu
-    
+
   echo ""
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   echo -e "\e[32mEnter a root password for KVM $hostname:\e[39m "
-  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   read root_password
 }
+
+function main_install {
 
   OS_ID=$(cat /etc/*release | grep VERSION_ID | cut -d\" -f2)
   echo "====================================================="
@@ -89,21 +48,22 @@ function questions {
   echo -e "\e[32mVirtualization host installation\e[39m"
   echo ""
   echo "This script will install all the necessary packages to use Libvirtd/KVM"
-  
+
   if [ "$OS_ID" == "18.04" ]; then
-    apt install -y qemu qemu-kvm libvirt-bin bridge-utils virt-manager virt-viewer libvirt-clients libvirt-daemon-system bridge-utils virt-top libguestfs-tools ; systemctl enable libvirtd ; systemctl start libvirtd
+    apt update ; apt upgrade -y ; apt install -y qemu qemu-kvm libvirt-bin bridge-utils virt-manager virt-viewer libvirt-clients libvirt-daemon-system bridge-utils virt-top libguestfs-tools ; systemctl enable libvirtd ; systemctl start libvirtd
    elif [ "$OS_ID" == "20.04" ]; then
-    apt install -y qemu qemu-kvm bridge-utils virt-manager virt-viewer libvirt-clients libvirt-daemon-system bridge-utils virt-top libguestfs-tools ; systemctl enable libvirtd ; systemctl start libvirtd
+    apt update ; apt upgrade -y ; apt install -y qemu qemu-kvm bridge-utils virt-manager virt-viewer libvirt-clients libvirt-daemon-system bridge-utils virt-top libguestfs-tools ; systemctl enable libvirtd ; systemctl start libvirtd
   elif [ "$OS_ID" == "8" ]; then
-    dnf install -y qemu-kvm qemu-img libvirt virt-install libvirt-client virt-top libguestfs-tools ; systemctl enable libvirtd ; systemctl start libvirtd
+    dnf update -y ; dnf install -y qemu-kvm qemu-img libvirt virt-install libvirt-client virt-top libguestfs-tools ; systemctl enable libvirtd ; systemctl start libvirtd
   elif [ "$OS_ID" == "7" ]; then
-    yum -y install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install libguestfs-xfs virt-top ; systemctl enable libvirtd ; systemctl start libvirtd
+    yum update -y ; yum -y install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install libguestfs-xfs virt-top ; systemctl enable libvirtd ; systemctl start libvirtd
   else
     echo "Unknown OS. Exiting..."
     exit 1
   fi
-  
-# This function should be implemented in the future
+
+}
+
 function e2fsck {
 
 e2fsck -V
@@ -129,7 +89,7 @@ questions
 #build Ubuntu 16.04 KVM guest:
 
 virt-builder ubuntu-16.04 \
---size=$size\G --format qcow2 -o /vmdata/$hostname.qcow2 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
 --hostname $hostname --network \
 --firstboot-command "dpkg-reconfigure openssh-server" \
 --edit '/etc/default/grub:
@@ -146,12 +106,16 @@ virt-install --import --name $hostname \
 --virt-type=qemu \
 --ram $ram \
 --vcpu $vcpu \
---disk path=/vmdata/$hostname.qcow2,format=qcow2 \
+--disk path=/vmdata/$hostname.img,format=raw \
 --os-variant ubuntu16.04 \
 --network=bridge=virbr0,model=virtio \
---noautoconsole
+--noautoconsole \
+--noreboot
 
 #sed -i 's/ens2/ens3/g' /etc/network/interfaces
+#virt-edit -d $hostname /etc/network/interfaces -e 's/ens2/ens3/g'
+#virt-edit -d kvm7 /etc/systemd/system/getty.target.wants/getty@tty1.service -e 's/"ExecStart=-/sbin/agetty --noclear %I $TERM"/"ExecStart=-/sbin/agetty --autologin root --noclear %I $TERM"/g'
+
 }
 
 #***************************
@@ -164,7 +128,7 @@ questions
 #build Ubuntu18.04 KVM guest:
 
 virt-builder ubuntu-18.04 \
---size=$size\G --format qcow2 -o /vmdata/$hostname.qcow2 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
 --hostname $hostname --network \
 --firstboot-command "dpkg-reconfigure openssh-server" \
 --edit '/etc/default/grub:
@@ -179,10 +143,11 @@ virt-install --import --name $hostname \
 --virt-type=qemu \
 --ram $ram \
 --vcpu $vcpu \
---disk path=/vmdata/$hostname.qcow2,format=qcow2 \
+--disk path=/vmdata/$hostname.img,format=raw \
 --os-variant ubuntu18.04 \
 --network=bridge=virbr0,model=virtio \
---noautoconsole
+--noautoconsole \
+--noreboot
 
 #sed -i 's/ens2/enp1s0/g' /etc/netplan/01-netcfg.yaml
 }
@@ -197,7 +162,7 @@ questions
 #build CentOS 7 KVM guest:
 
 virt-builder centos-7.7 \
---size=$size\G --format qcow2 -o /vmdata/$hostname.qcow2 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
 --hostname $hostname --network \
 --root-password password:$root_password --no-check-signature
 
@@ -207,11 +172,12 @@ virt-install --import \
 --name $hostname \
 --virt-type=qemu \
 --ram $ram \
---disk path=/vmdata/$hostname.qcow2 \
+--disk path=/vmdata/$hostname.img \
 --vcpus $vcpu \
 --os-variant centos7.0 \
 --network=bridge=virbr0,model=virtio \
---noautoconsole
+--noautoconsole \
+--noreboot
 
 }
 
@@ -225,7 +191,7 @@ questions
 #install CentOS 8 KVM guest:
 
 virt-builder centos-8.2 \
---size=$size\G --format qcow2 -o /vmdata/$hostname.qcow2 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
 --hostname $hostname --network \
 --root-password password:$root_password --no-check-signature
 
@@ -235,11 +201,12 @@ virt-install --import \
 --name $hostname \
 --virt-type=qemu \
 --ram $ram \
---disk path=/vmdata/$hostname.qcow2 \
+--disk path=/vmdata/$hostname.img \
 --vcpus $vcpu \
 --os-variant centos8 \
 --network=bridge=virbr0,model=virtio \
---noautoconsole
+--noautoconsole \
+--noreboot
 }
 
 #***************************
@@ -252,7 +219,7 @@ questions
 #build Debian 9 KVM guest:
 
 virt-builder debian-9 \
---size=$size\G --format qcow2 -o /vmdata/$hostname.qcow2 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
 --hostname $hostname --network \
 --firstboot-command "dpkg-reconfigure openssh-server" \
 --edit '/etc/default/grub:
@@ -267,10 +234,11 @@ virt-install --import --name $hostname \
 --virt-type=qemu \
 --ram $ram \
 --vcpu $vcpu \
---disk path=/vmdata/$hostname.qcow2,format=qcow2 \
+--disk path=/vmdata/$hostname.img,format=raw \
 --os-variant debian9 \
 --network=bridge=virbr0,model=virtio \
---noautoconsole
+--noautoconsole \
+--noreboot
 
 }
 
@@ -286,7 +254,7 @@ questions
 #build Debian 10 KVM guest:
 
 virt-builder debian-10 \
---size=$size\G --format qcow2 -o /vmdata/$hostname.qcow2 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
 --hostname $hostname --network \
 --firstboot-command "dpkg-reconfigure openssh-server" \
 --edit '/etc/default/grub:
@@ -301,16 +269,115 @@ virt-install --import --name $hostname \
 --virt-type=qemu \
 --ram $ram \
 --vcpu $vcpu \
---disk path=/vmdata/$hostname.qcow2,format=qcow2 \
+--disk path=/vmdata/$hostname.img,format=raw \
 --os-variant debian10 \
 --network=bridge=virbr0,model=virtio \
---noautoconsole
+--noautoconsole \
+--noreboot
 
 }
 
 #sed -i 's/ens2/enp1s0/g' /etc/network/interfaces
 
 #***************************
+
+function fedora31 {
+
+#enter variables by answering the questions
+questions
+
+#install Fedora 31 KVM guest:
+
+virt-builder fedora-31 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
+--hostname $hostname --network \
+--root-password password:$root_password --no-check-signature
+
+#install Fedora 31 KVM guest:
+
+virt-install --import \
+--name $hostname \
+--virt-type=qemu \
+--ram $ram \
+--disk path=/vmdata/$hostname.img \
+--vcpus $vcpu \
+--os-type linux \
+--os-variant fedora31 \
+--network=bridge=virbr0,model=virtio \
+--noautoconsole \
+--noreboot
+}
+
+#***************************
+
+function fedora32 {
+
+#enter variables by answering the questions
+questions
+
+#install Fedora 32 KVM guest:
+
+virt-builder fedora-32 \
+--size=$size\G --format raw -o /vmdata/$hostname.img \
+--hostname $hostname --network \
+--root-password password:$root_password --no-check-signature
+
+#install Fedora 32 KVM guest:
+
+virt-install --import \
+--name $hostname \
+--virt-type=qemu \
+--ram $ram \
+--disk path=/vmdata/$hostname.img \
+--vcpus $vcpu \
+--os-variant fedora31 \
+--network=bridge=virbr0,model=virtio \
+--noautoconsole \
+--noreboot
+}
+
+#***************************
+
+function main {
+
+    declare -A selection;
+    declare -a items;
+
+    selection[ubuntu16]="  Ubuntu 16.04  "; items+=( ubuntu16 )
+    selection[ubuntu20]="  Ubuntu 20.04  "; items+=( ubuntu20 )
+    selection[centos7]="  CentOS 7  "; items+=( centos7 )
+    selection[centos8]="  CentOS 8  "; items+=( centos8 )
+    selection[debian9]="  Debian 9  "; items+=( debian9 )
+    selection[debian10]="  Debian 10  "; items+=( debian10 )
+    selection[fedora31]="  Fedora 31  "; items+=( fedora31 )
+    selection[fedora32]="  Fedora 32  "; items+=( fedora32 )
+
+    entry_options=()
+    entries_count=${#selection[@]}
+    message=$'Choose what to be installed. The below choices can be selected.\n\nInstallation list:\n\n'
+    choices=()
+
+    for i in "${items[@]}"
+    do
+        entry_options+=("$i")
+        entry_options+=("${selection[$i]}")
+        entry_options+=("ON")
+    done
+
+    selected_installations=$(whiptail --checklist --separate-output --title "Choose services" "$message" 20 60 $entries_count -- "${entry_options[@]}" 3>&1 1>&2 2>&3)
+    mapfile -t choices <<< "$selected_installations"
+
+    # Install qemu-kvm
+    main_install
+
+    for selected in "${choices[@]}"; do
+        # run all selected functions
+        $selected
+    done
+}
+
+# Call main function
+main
 
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
 echo -e "\e[32mInstallation complete.\e[39m"
